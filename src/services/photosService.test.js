b25f507989c4416344697d5eb13172aa981c1b1d
Haosik <noobbaster@gmail.com>
2019-07-19 16:53:35 +0300

    small photoService test improvements

--- src/services/photosService.test.js
+++ src/services/photosService.test.js
@@ -1,8 +1,12 @@
 import { sortByAlbum, getPhotosFromApi } from './photosService';
+import { TOTAL_IMAGES } from './../constants/api';
 
 describe('getPhotosFromApi', () => {
-  it('should make api call and return data if api responded', async () => {
+  it('should make API call and return data if api responded', async () => {
     expect(await getPhotosFromApi()).toMatchSnapshot();
+  });
+  it('should return limited number of items from API', async () => {
+    expect(await getPhotosFromApi()).toHaveLength(TOTAL_IMAGES);
   });
 });
 
@@ -30,33 +34,36 @@ describe('sortByAlbum', () => {
       url: 'https://via.placeholder.com/600/771796'
     }
   ];
+
+  const expected = {
+    1: [
+      {
+        albumId: 1,
+        id: 1,
+        thumbnailUrl: 'https://via.placeholder.com/150/92c952',
+        title: 'accusamus beatae ad facilis cum similique qui sunt',
+        url: 'https://via.placeholder.com/600/92c952'
+      }
+    ],
+    99: [
+      {
+        albumId: 99,
+        id: 88,
+        thumbnailUrl: 'https://via.placeholder.com/150/771796',
+        title: 'reprehenderit est deserunt velit ipsam',
+        url: 'https://via.placeholder.com/600/771796'
+      },
+      {
+        albumId: 99,
+        id: 199,
+        thumbnailUrl: 'https://via.placeholder.com/150/771796',
+        title: 'reprehenderit est deserunt velit ipsam',
+        url: 'https://via.placeholder.com/600/771796'
+      }
+    ]
+  };
+
   it('should return sorted by albumId object ', () => {
-    expect(sortByAlbum(photos)).toEqual({
-      1: [
-        {
-          albumId: 1,
-          id: 1,
-          thumbnailUrl: 'https://via.placeholder.com/150/92c952',
-          title: 'accusamus beatae ad facilis cum similique qui sunt',
-          url: 'https://via.placeholder.com/600/92c952'
-        }
-      ],
-      99: [
-        {
-          albumId: 99,
-          id: 88,
-          thumbnailUrl: 'https://via.placeholder.com/150/771796',
-          title: 'reprehenderit est deserunt velit ipsam',
-          url: 'https://via.placeholder.com/600/771796'
-        },
-        {
-          albumId: 99,
-          id: 199,
-          thumbnailUrl: 'https://via.placeholder.com/150/771796',
-          title: 'reprehenderit est deserunt velit ipsam',
-          url: 'https://via.placeholder.com/600/771796'
-        }
-      ]
-    });
+    expect(sortByAlbum(photos)).toEqual(expected);
   });
 });
